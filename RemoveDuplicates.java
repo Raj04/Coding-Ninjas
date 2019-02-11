14af705398ca825c8473fc34559b48e64228315c
Raj_Yadav <raj.yadav@aot.edu.in>
2019-02-11 10:28:00 +0530

    Remove Duplicates using Recursively

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,2 +1,33 @@
 
-#pending
+import java.util.Scanner;
+
+public class Main {
+	
+	static Scanner s = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+    	String input = s.next();
+    	System.out.println(Solution.removeConsecutiveDuplicates(input));
+    }
+}
+
+
+public class Solution {
+
+	public static String removeConsecutiveDuplicates(String s) {
+		// Write your code here
+	
+      	String str="";
+      	if(s.length()==1){
+          str=s;
+          return str;
+        }
+	    String actual=removeConsecutiveDuplicates(s.substring(0,s.length()-1));
+      	if(s.charAt(s.length()-1)==actual.charAt(actual.length()-1)){
+          return actual;
+        }
+     	actual+=Character.toString(s.charAt(s.length()-1));
+      	return actual;
+	}
+
+}
