ba86fffa191ae1c59308f8ad7d5cef20e7f219b6
Raj_Yadav <raj.yadav@aot.edu.in>
2019-02-16 12:30:19 +0530

    Quick Sort Solution(best learning experience)

--- QuickSort.java
+++ QuickSort.java
@@ -1,1 +1,85 @@
 
+import java.util.Scanner;
+
+public class runner {
+	public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+		int size = s.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void printArray(int input[]) {
+		for(int i = 0; i < input.length; i++) {
+			System.out.print(input[i] + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] input = takeInput();
+		solution.mergeSort(input);
+		printArray(input);
+	}
+}
+
+public class solution {
+
+	public static void mergeSort(int[] input){
+		merging(input,0,input.length-1);
+	}
+  	public static void merging(int []input,int start,int end){
+      if(start<end){
+        int mid=(start+end)/2;
+        merging(input,start,mid);
+        merging(input,mid+1,end);
+        merge(input,start,mid,end);
+      }
+    }
+  	public static void merge(int arr[], int l, int m, int r) 
+    { 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        /* Create temp arrays */
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+  
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i) 
+            L[i] = arr[l + i]; 
+        for (int j=0; j<n2; ++j) 
+            R[j] = arr[m + 1+ j]; 
+  
+  
+        int i = 0, j = 0; 
+  
+        // Initial index of merged subarry array 
+        int k = l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        }   
+        /* Copy remaining elements of L[] */
+        while (i < n1) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+        
+    } 
+}
