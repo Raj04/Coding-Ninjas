3b4276dc72235f9767ba1cb76c74604433b1a973
Raj_Yadav <raj.yadav@aot.edu.in>
2019-02-23 19:06:01 +0530

    Append last n elements to first

---
+++ LinkedList/Programs/Append/Append.java
@@ -0,0 +1,49 @@
+
+
+/*
+class LinkedListNode<T> {
+	public T data;
+	public LinkedListNode<T> next;
+
+	public LinkedListNode(T data) {
+		this.setData(data);
+		this.next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+}
+* */
+
+
+public class Solution {
+	public static LinkedListNode<Integer> append(LinkedListNode<Integer> root, int n) {
+      LinkedListNode<Integer> temp=null;
+      LinkedListNode<Integer> end=null;
+      int length=1;
+      end=root;
+      //pointing this end node to last node
+      while(end.next!=null){
+        length++;
+        end=end.next;
+      }
+      int i=1;
+      temp=root;
+      //pointing this temp node to the (length-2)node i.e the node after this we have to append 
+      //if n==2 then we will point to 8th node
+      while(i<length-n){
+        i++;
+        temp=temp.next;
+      }
+      end.next=root;
+      root=temp.next;
+      temp.next=null;
+      return root;
+    }
+}
