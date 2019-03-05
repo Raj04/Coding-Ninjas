31d55a1cd6b878aaa0893e8d4d60b3fcdcbe7f72
Raj_Yadav <raj.yadav@aot.edu.in>
2019-03-05 10:36:12 +0530

    Find nodes who doesn't have a siblings

---
+++ BinaryTree/NodesWithoutSibling.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+
+/*	Binary Tree Node class
+ * 
+ * class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+		
+		public BinaryTreeNode(T data) {
+			this.data = data;
+		}
+	}
+	*/
+	
+	public static int sum(BinaryTreeNode<Integer> root){
+		
+		// Write your code here
+        if(root==null){
+            return 0;
+        }
+        int sum=root.data+sum(root.left)+sum(root.right);
+        return sum;
+	}
+	
+}
