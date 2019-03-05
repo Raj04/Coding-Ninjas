5910eec5bbcf702d813e063f1eb9b6224ea8fe38
Raj_Yadav <raj.yadav@aot.edu.in>
2019-03-05 11:07:26 +0530

    Create the mirror view of a Binary Tree

---
+++ BinaryTree/mirrorView.java
@@ -0,0 +1,39 @@
+
+#pending
+
+
+public class Solution {
+
+/*	Binary Tree Node class
+ * 
+ * class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+		
+		public BinaryTreeNode(T data) {
+			this.data = data;
+		}
+	}
+	*/
+	
+	public static void mirror(BinaryTreeNode<Integer> root){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * No need to print or return the output.
+		 * Taking input and printing output is handled automatically.
+		 */
+            
+        BinaryTreeNode<Integer> temp=null;
+        if(root==null){
+            return ;
+        }
+        mirror(root.left);
+        mirror(root.right);
+        temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+	}
+	
+}
