a8fcc8de147c8bbd045bce58f1fcc691cb2894da
Raj_Yadav <raj.yadav@aot.edu.in>
2019-03-06 09:14:38 +0530

    Find the height of the tree

---
+++ BinaryTree/Height.java
@@ -0,0 +1,39 @@
+#find the height of the tree
+/*
+no of nodes from leaf to root(longest route)
+*/
+public class Solution {
+
+/*	Binary Tree Node class
+ * 
+ * class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+		
+		public BinaryTreeNode(T data) {
+			this.data = data;
+		}
+	}
+	*/
+	public static int height(BinaryTreeNode<Integer> root) {
+		/* Your class should be named Solution
+		* Don't write main().
+		* Don't read input, it is passed as function argument.
+		* Return output and don't print it.
+		* Taking input and printing output is handled automatically.
+		*/
+	    
+        if(root==null){
+            return 0;
+        }
+        int ldepth=height(root.left);
+        int rdepth=height(root.right);
+        if(ldepth>rdepth){
+            return ldepth+1;
+        }else{
+            return rdepth+1;
+        }
+	}
+	
+}
