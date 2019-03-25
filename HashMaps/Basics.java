5364676cf08b75f7f9854f7c59359ae1b46039c5
Raj_Yadav <raj.yadav@aot.edu.in>
2019-03-25 23:56:51 +0530

    Basic operations get,put, isEmpty( ),size()

--- HashMaps/Basics.java
+++ HashMaps/Basics.java
@@ -1,1 +1,21 @@
-#pending
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    
+	public static void main (String[] args) {
+	   
+	   Map<String,Integer> mp=new HashMap<>();
+	   mp.put("abc",1);
+	   mp.put("def",2);
+	   mp.put("ghi",3);
+	   mp.put("jkl",4);
+	   
+	   System.out.println(mp.get("jkl"));//to get the value
+	   System.out.println(mp.isEmpty());//checking the hashmap is empty or TypeNotPresentException
+	   System.out.println("The actual hashmap size "+ mp.size());//return the size
+	   System.out.println(mp.remove("jkl"));//remove returns the deleted key
+	   System.out.println("The actual hashmap size after deleting one key "+ mp.size());//return the size
+	   
+	}
+}
