290b1f09a8f990c832f4313f6747572906f98836
MSTE CRD <110633697+mstecrd@users.noreply.github.com>
2022-10-19 01:12:34 +0530

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n = 6;
+    printf("%d", fib(n));
+    return 0;
+}
