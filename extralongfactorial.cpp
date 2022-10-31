7c83dd2435a48ed023057c8610cb39e005417e03
vivekthakur8102000 <44321333+vivekthakur8102000@users.noreply.github.com>
2022-10-31 11:17:11 +0530

    Create extralongfactorial.cpp

---
+++ extralongfactorial.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[10000]={-1};
+    int end=0;
+    int carry=0;
+    a[0]=1;
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=0;j<=end;j++)
+        {
+              int mul=a[j]*i+carry;
+              a[j]=mul%10;
+              carry=mul/10;
+        }
+        while(carry!=0)
+        {
+            a[++end]=carry%10;
+            carry=carry/10;
+        }
+    }
+    for(int i=end;i>=0;i--)
+    {
+        cout<<a[i];
+    }
+}
